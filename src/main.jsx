2dd0326c12e919845fa1f518c4fca71998dcf952
gato8989 <115903430+gato8989@users.noreply.github.com>
2025-05-05 21:23:16 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-<<<<<<< HEAD
 
-=======
->>>>>>> parent of d0e737d (Cambios de implementaciones)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
-<<<<<<< HEAD
 
-reportWebVitals();
-=======
->>>>>>> parent of d0e737d (Cambios de implementaciones)
